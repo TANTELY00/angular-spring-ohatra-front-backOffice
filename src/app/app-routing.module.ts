d614edb33f0c9d42f68ed554ad6c00bff188cd99
TANTELINIRINARAVOSON <tantelyravoson2019@gmail.com>
2024-09-26 09:18:55 +0300

    deuxieme version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ServiceComponent } from './service/service.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ClientComponent } from './client/client.component';
+import { CommandeComponent } from './commande/commande.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"dashboard",component:DashboardComponent},
+  {path:"service" ,component:ServiceComponent},
+  {path:"categorie",component:CategorieComponent},
+  {path:"client" ,component:ClientComponent},
+  {path:"produit" ,component:ProduitComponent},
+  {path:"commande" ,component:CommandeComponent},
+  {path:"profile" ,component:ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { CategorieComponent } from './categorie/categorie.component';
+import { ServiceComponent } from './service/service.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ClientComponent } from './client/client.component';
+import { CommandeComponent } from './commande/commande.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent,
+    CategorieComponent,
+    ServiceComponent,
+    ProduitComponent,
+    ClientComponent,
+    CommandeComponent,
+    ProfileComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     provideAnimationsAsync()
